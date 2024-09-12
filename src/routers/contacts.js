0df727442f4038ba7b262b1b691bc7a8cc728728
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2024-09-13 00:23:47 +0200

    finish

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,6 +3,12 @@ import { Router } from 'express';
 import * as contactsControllers from '../controllers/contacts.js';
 
 import ctrlWrapper from '../utils/ctrlWrapper.js';
+import validateBody from '../utils/validateBody.js';
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+} from '../validation/contacts.js';
+import isValidId from '../middlewares/isValidId.js';
 
 const contactsRouter = Router();
 
@@ -12,17 +18,25 @@ contactsRouter.get(
 );
 contactsRouter.get(
   '/:id',
+  isValidId,
   ctrlWrapper(contactsControllers.getContactByIdController),
 );
-contactsRouter.post('/', ctrlWrapper(contactsControllers.addContactController));
+contactsRouter.post(
+  '/',
+  validateBody(contactAddSchema),
+  ctrlWrapper(contactsControllers.addContactController),
+);
 
 contactsRouter.patch(
   '/:id',
+  isValidId,
+  validateBody(contactUpdateSchema),
   ctrlWrapper(contactsControllers.pathContactController),
 );
 
 contactsRouter.delete(
   '/:id',
+  isValidId,
   ctrlWrapper(contactsControllers.deleteContactController),
 );
 export default contactsRouter;
