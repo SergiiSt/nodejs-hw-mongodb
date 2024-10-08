ce06c9b9a9e35dec47aafe677678615a8bcd72f5
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2024-10-08 19:53:15 +0200

    bug fix 08.10.2024

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -24,13 +24,13 @@ authRouter.post(
 authRouter.post('/refresh', ctrlWrapper(authControllers.refreshController));
 authRouter.post('/logout', ctrlWrapper(authControllers.signoutController));
 authRouter.post(
-  '/request-reset-email',
+  '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController),
 );
 
 authRouter.post(
-  '/reset-password',
+  '/reset-pwd',
   validateBody(resetPasswordSchema),
   ctrlWrapper(resetPasswordController),
 );
